74a5a3ebb874f33ab1e2076437cb812b69fa9fda
InventedMost <netvid00@gmail.com>
2022-08-13 10:25:22 +0100

    updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import vitalRoute from "./routes/vitalRoutes.js";
 import dosageRoute from "./routes/dosageRoutes.js";
 import conditionRoute from "./routes/conditionRoutes.js";
 import cors from "cors";
+import morgan from "morgan";
 
 import configDB from "./config/db.js";
 
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(morgan("tiny"));
 app.listen(port, () => {
   console.log(`server live on port ${port}`);
 });
